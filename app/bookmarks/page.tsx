6c7ca8afc1804117128eb67650e38ecaab37e2bf
MohamedRasheqA <rasheq@klmsolutions.in>
2025-06-13 04:07:43 +0530

    Commit

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1283,7 +1283,7 @@ export default function BookmarksPage() {
 
         {/* Main Content Area */}
         <main className="flex-1 min-h-0 flex flex-col px-4 md:px-8 bg-[#f5f8fa]">
-          <div className="flex-1 overflow-y-auto space-y-4 pb-24 md:pb-4">
+          <div className="flex-1 overflow-y-auto space-y-4 pb-32 md:pb-8">
             {searchPerformed ? (
               <div className="space-y-6">
                 {/* Content Type Filters and Sort Options in Same Row */}
